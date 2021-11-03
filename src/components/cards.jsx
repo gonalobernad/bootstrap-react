2ad64d7789c476cc634cacae39efc15ec542f9dc
Gonzalo Bernad <gonzalobernad60@gmail.com>
2021-11-03 20:47:03 -0300

    Instalación bootstrap 4.1

---
+++ src/components/cards.jsx
@@ -0,0 +1,13 @@
+import Card from './card';
+
+const Cards = () => {
+    return(
+        <div className="d-flex flex-row justify-content-around">
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    );
+}
+
+export default Cards;
